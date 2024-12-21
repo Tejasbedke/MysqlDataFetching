1c6f9fd009bc9f4ac27cfe97f749f9e331c48772
Tejas <tejasbedke@gmail.com>
2024-12-21 21:39:25 +0530

    TL modified controller on branch1

--- FetchingData/src/com/tej/Services/Controller.java
+++ FetchingData/src/com/tej/Services/Controller.java
@@ -10,4 +10,12 @@ public class Controller {
 	public void m2() {
 		System.out.println("Controller.m2()");
 	}
+	
+	public void m3() {
+		System.out.println("Controller.m3()");
+		
+	}
+	public void m4() {
+		System.out.println("Controller.m4()");
+	}
 }
