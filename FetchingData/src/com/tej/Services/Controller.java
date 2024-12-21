11aaf8225770692f8de53605efccbf5c293822a4
Tejas <tejasbedke@gmail.com>
2024-12-21 21:31:06 +0530

    Dev1 Controller modified

--- FetchingData/src/com/tej/Services/Controller.java
+++ FetchingData/src/com/tej/Services/Controller.java
@@ -2,4 +2,12 @@ package com.tej.Services;
 
 public class Controller {
 
+	public void m1() {
+		System.out.println("Controller.m1()");
+		
+	}
+	
+	public void m2() {
+		System.out.println("Controller.m2()");
+	}
 }
