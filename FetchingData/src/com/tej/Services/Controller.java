1edf418b660b5a8c15c496178ad3e3257f3b6983
Tejas <tejasbedke@gmail.com>
2024-12-21 08:16:33 +0530

    Package initializing

---
+++ FetchingData/src/com/tej/Services/Controller.java
@@ -0,0 +1,5 @@
+package com.tej.Services;
+
+public class Controller {
+
+}
---
+++ FetchingData/src/com/tej/test/EnduserInteraction.java
@@ -0,0 +1,10 @@
+package com.tej.test;
+
+public class EnduserInteraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
